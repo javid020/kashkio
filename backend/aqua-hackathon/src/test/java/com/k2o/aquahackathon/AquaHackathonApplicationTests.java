084415ab50227951fdf8649575ec4d86d0d533e3
jhasanov <jhasanov@ada.edu.az>
2020-07-03 18:52:30 +0400

    SpringBoot project init

---
+++ backend/aqua-hackathon/src/test/java/com/k2o/aquahackathon/AquaHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k2o.aquahackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

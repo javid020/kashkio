412bdf79feeb4b8d20402576c2240e1af9c38347
Javid Abbasov <lut_milcey@mail.ru>
2020-07-04 20:10:31 +0400

    added rating page

---
+++ mobile/kashkio/Controllers/RatingViewController.swift
@@ -0,0 +1,54 @@
+//
+//  RatingViewController.swift
+//  test
+//
+//  Created by Javid Abbasov on 04.07.2020
+//  Copyright © 2020 Javid Abbasov. All rights reserved.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+    
+    @IBOutlet weak var ratingTableView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
+
+extension RatingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return istekler.count
+    }
+    
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ratingCell") as? RatingTableViewCell else { return UITableViewCell()}
+        
+//        cell.setup(istek: istekler[indexPath.row], id: indexPath.row)
+        cell.istek = istekler[indexPath.row]
+        cell.selectionStyle = .none
+
+        return cell
+        
+    }
+    
+    
+    
+    
+}
+
